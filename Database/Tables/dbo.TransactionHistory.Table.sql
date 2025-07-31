828724cdc8e95ec254116ba579ff9265c8fbb33e
AStowell <Andrew.Stowell@Allscripts.com>
2025-07-31 13:24:49 -0400

    Add RecordedDTTM column to dbo.TransactionHistory table

--- Database/Tables/dbo.TransactionHistory.Table.sql
+++ Database/Tables/dbo.TransactionHistory.Table.sql
@@ -1,15 +1,19 @@
 /****
   Holds a history of the users financial transactions.
+  
+  History:
+    - Added Column RecordedDTTM to track when the record was added to the table.
 ****/
 
 IF NOT EXISTS ( SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'TransactionHistory' )
 BEGIN
 CREATE TABLE dbo.TransactionHistory
 (
-     AccountId INT                NOT NULL 
-	,Debit     MONEY                  NULL
-	,Credit    MONEY                  NULL
+     AccountId       INT          NOT NULL 
+	,Debit           MONEY            NULL
+	,Credit          MONEY            NULL
 	,TransactionDTTM DATETIME2(0) NOT NULL
+    ,RecordedDTTM    DATETIME2(0) NOT NULL
 	,FOREIGN KEY ( AccountId) 
 	 REFERENCES dbo.Account
 );
